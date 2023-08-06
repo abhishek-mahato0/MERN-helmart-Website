46913b27af6457d989a73fe48a2738da649dee76
Abhishek Mahato <101643561+abhishek-mahato0@users.noreply.github.com>
2023-08-06 10:56:02 +0545

    error fixed

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -12,7 +11,5 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-    
   </React.StrictMode>
 );
-reportWebVitals();
